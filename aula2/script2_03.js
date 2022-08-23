66f0afb0948ce008378bfb8a95164ea432ff870e
Hebert <hebertphp@gmail.com>
2022-08-22 22:05:49 -0300

    PFE_2a_N

---
+++ aula2/script2_03.js
@@ -0,0 +1,3 @@
+document.write("6 + 19 = <h2>",6 + 19,"</h2>");
+alert("teste de \nquebra de linha");
+console.log("Onde vejo isso?");
---
+++ aula2/script2_05.js
@@ -0,0 +1,21 @@
+document.getElementById("teste").innerHTML="Funciona?<hr>";
+document.querySelector(".teste1").innerHTML="Será que funciona?";
+document.querySelector("#obs").innerHTML="Observação"; 
+document.querySelector("h1").innerHTML="Segunda aula bem diferente";
+document.querySelector("h3").innerHTML="Mudou";
+/*
+Resumo:
+HTML            CSS/JS
+id              #
+class           .
+nome_da_tag     nome_da_tag
+*/
+
+
+//escreve algo
+/*
+posso escrever 
+em 
+várias 
+Linhas
+*/
