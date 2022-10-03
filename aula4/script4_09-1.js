a874265ff91de8203fdc5562ab16014348a4b03a
Hebert <hebertphp@gmail.com>
2022-10-03 11:40:36 -0300

    PFE_2a_N 26/09/22

---
+++ aula4/script4_09-1.js
@@ -0,0 +1,12 @@
+let a,b;//
+a=10;     //10 int
+b="10";   //10 string
+document.write("Igualdade = "+(a==b)+"<br>");//TRUE (V)
+document.write("Idêntico = "+(a===b)+"<br>");//FALSE (F)
+document.write("Não igual = "+(a!=b)+"<br>");//FALSE (F)
+document.write("Não Idêntico = "+(a!==b)+"<br>");//TRUE (V)
+/*
+=   receber/atribuir
+==  igualdade
+=== idênticos
+*/
---
+++ aula4/script4_duvida1.js
@@ -0,0 +1,7 @@
+let x = "Hoje tem atividade";
+let tag= "h1";
+tag="h2";
+document.write("Será que " + x + "?<br>");
+document.write(`Será que  ${x}? <br>`);
+document.write('Será que  ${x}? <br>');
+document.write(`< ${tag}> Será que  ${x}? < /${tag}>`);
