f6bc1b299f3a90bbf8f199c4db5e69826fae1dd6
Hebert <hebertphp@gmail.com>
2022-10-17 21:01:41 -0300

    17/10/22

---
+++ aula5/script5_par_impar-1.js
@@ -0,0 +1,7 @@
+/*
+Elaborar um código Javascript para dado um número inteiro, verificar se
+o número é PAR ou ÍMPAR. A saída deverá mostrar o número e mostrar
+PAR ou ÍMPAR, como mostrado a seguir:
+275 - ÍMPAR
+28 - PAR
+*/
