e778d98ffcea4176db84043ba2b4dceaab85462a
Hebert <hebertphp@gmail.com>
2022-10-17 21:24:53 -0300

    17/10/22

--- aula5/script5_par_impar-1.js
+++ aula5/script5_par_impar-1.js
@@ -1,3 +1,17 @@
+let num,calc;
+const saida=document.querySelector(".saida");
+
+num=parseInt(prompt("Digite um numero inteiro"));
+calc=num%2;
+
+if(calc==0){
+    saida.innerHTML=`${num} - Par`;
+}
+else{
+    saida.innerHTML=`${num} - Ímpar`;
+}
+
+//saida.innerHTML=`num=${num} calc=${calc}`;
 /*
 Elaborar um código Javascript para dado um número inteiro, verificar se
 o número é PAR ou ÍMPAR. A saída deverá mostrar o número e mostrar
