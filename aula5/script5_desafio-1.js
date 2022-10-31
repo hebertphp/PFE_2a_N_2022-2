37f548245a2e39947b2714cd28c8287dbac53786
Hebert <hebertphp@gmail.com>
2022-10-31 20:15:35 -0300

    31/10/22

---
+++ aula5/script5_desafio-1.js
@@ -0,0 +1,9 @@
+/*
+Solicitar a entrada de duas notas (av1 e av2)
+Calcular a média das duas notas e mostrar:
+    O valor de cada nota
+    A média e:
+        "Aprovado"
+        "Reprovado"
+Pensar, qual é a condição para esse caso? >=6        
+*/
--- aula5/script5_par_impar-3.js
+++ aula5/script5_par_impar-3.js
@@ -1,5 +1,5 @@
-let num,res;
+let num,msg;
 const saida=document.querySelector(".saida");
 num=parseInt(prompt("Digite um número"));
-msg=num%2==0?"PAR":"ÍMPAR";
+msg=num%2==0?"PAR":"ÍMPAR";//ternário
 saida.innerHTML=`${num} - ${msg}`;
