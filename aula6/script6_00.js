3a2690148ee3df2b2bd10412c8eb28099da1ea84
Hebert <hebertphp@gmail.com>
2022-11-21 20:25:19 -0300

    21/11/22

---
+++ aula6/script6_00.js
@@ -0,0 +1,12 @@
+const saida=document.querySelector(".saida");
+let i,x,uf;
+
+i=10;
+i=20;
+i=30;
+i=2;
+
+x=[10,20,30,2,"teste"];//array
+uf=["SP","RJ","SC","MG"];
+
+saida.innerHTML=`${i}<hr>${x}<br>${uf}`;
