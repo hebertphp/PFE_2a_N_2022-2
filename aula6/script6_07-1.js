05a2501319da2a0a467f7ebebf25ca70ab15ff7b
Hebert <hebertphp@gmail.com>
2022-12-05 20:42:13 -0300

    05/12/2022

--- aula6/script6_07-1.js
+++ aula6/script6_07-1.js
@@ -1,14 +1,22 @@
 const saida=document.querySelector('.saida');
+const lista=document.querySelector('.lista');
+let teste="";
 const estudante=['Astrogildo','Belarmina','Pafuncia','Abelarda','Limonselo','Giva','Jubri','Diva'];
 const produtos=["mouse","teclado","monitor","headset","café","celular"];
 //for (let var_simples of var_tipo_array){
 //  var_simples serve para pegar cada valor individual do array
 // um valor de cada vez
 //}
+// saida.innerHTML+=`< ol>`;
+teste="<ol>";
 for (let x of estudante){
-    saida.innerHTML+=`${x}<br>`;
+    teste+=`<li>${x}</li>`;
 }
+teste+="</ol>";
+saida.innerHTML=teste;
+//  saida.innerHTML+=`< /ol>`;
 
+//  saida.innerHTML=`<ol><li>Astrogildo</li><li>Belarmina</li></ol>`;
 /*
 x="mouse";saida.innerHTML+=`${x}<br>`;//imprime mouse
 x="teclado";saida.innerHTML+=`${x}<br>`;//imprime apenas teclado
