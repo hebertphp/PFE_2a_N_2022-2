c09bba287dcd5b77d7e217b76c436f319b18c604
Hebert <hebertphp@gmail.com>
2022-12-05 19:51:55 -0300

    05/12/22

--- aula6/script6_06-2.js
+++ aula6/script6_06-2.js
@@ -1,8 +1,9 @@
 const saida=document.querySelector('.saida');
-const mes_select=document.querySelector("#mes");
-//                0        1         2        3         4                
-const produtos=["mouse","teclado","monitor","headset","café","celular"];
+// const mes_select=document.querySelector("#mes");
+//                0        1         2        3         4         5       
+const produtos=["mouse","teclado","monitor","headset","café","celular","tablet","impressora"];
 
-for(let i=0;i<5;i++){
+for(let i=0;i<6;i++){
     saida.innerHTML+=`${produtos[i]} <br>`;
 }
+saida.innerHTML=produtos.length;
--- aula6/script6_07-1.js
+++ aula6/script6_07-1.js
@@ -1,6 +1,10 @@
 const saida=document.querySelector('.saida');
-const estudante=['Astrogildo','Belarmina','Pafuncia'];
-
-for (let n of estudante){
-    saida.innerHTML+=`${n}<br>`;
+const estudante=['Astrogildo','Belarmina','Pafuncia','Abelarda','Limonselo','Giva','Jubri','Diva'];
+const produtos=["mouse","teclado","monitor","headset","café","celular"];
+//for (let var_simples of var_tipo_array){
+//  var_simples serve para pegar cada valor individual do array
+// um valor de cada vez
+//}
+for (let var_qq of produtos){
+    saida.innerHTML+=`${var_qq}<br>`;
 }
