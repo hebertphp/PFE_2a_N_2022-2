0b9edce3c05717c1abe2b8cfe2db6af8e8789c5a
Hebert <hebertphp@gmail.com>
2022-12-12 19:55:36 -0300

    12/12/22

---
+++ aula7/aula7_01.js
@@ -0,0 +1,11 @@
+/* -----------------------------------------
+ function boas_vindas()
+ Objetivo: Enviar mensagem de boas vindas
+ Data de criação: 12/12/2022
+ Autor: Nome do autor
+-------------------------------------------- */
+const saida=document.querySelector('.saida');
+function boas_vindas() {
+    saida.innerHTML+="Seja bem vindo!";
+}
+//boas_vindas();
